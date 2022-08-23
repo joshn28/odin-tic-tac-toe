baa832ac327a45df5b0685b0fc57f8608cd03418
Joshua Nobleza <joshnobleza@bell.net>
2022-08-23 14:32:06 -0400

    Add logic which checks for 3-in-a-row

--- js/script.js
+++ js/script.js
@@ -4,14 +4,51 @@ const gameBoard = (() => {
     'strict mode';
 
     const _board = [
-        ['X', 'O', 'X'],
-        ['X', 'O', 'X'],
+        ['X', 'X', 'X'],
+        ['X', '', 'X'],
         ['X', 'O', 'X']
     ];
+    const _threeInARow = (mark) => {
+        let win = false;
+
+        _board.forEach(row => {
+
+            if (row.every(el => mark === el)) {
+                win = true;
+                return;
+            }
+        });
+
+        return win;
+    };
+    let playerTurn = "X";
     const getBoard = () => _board;
+    const checkWin = (mark) => {
+        if (_threeInARow(mark)) {
+            return true;
+        }
+    }
+
+    document.addEventListener('click', function(e) {
+        if (!e.target.textContent) {
+            const rowNumber = e.target.dataset.row;
+            const colNumber = e.target.dataset.column;
+
+            _board[rowNumber][colNumber] = playerTurn;
+
+            e.target.textContent = playerTurn;
+
+            if (playerTurn === "X") {
+                playerTurn = "O";
+            } else {
+                playerTurn = "X";
+            }
+        }
+    });
 
     return {
-        getBoard
+        getBoard,
+        checkWin
     };
 })();
 
@@ -38,19 +75,23 @@ const displayController = (() => {
     };
 })();
 
-const player = (name, mark) => {
+const player = (name, marker) => {
     let wins = 0;
     const getName = () => name;
     const getNumberOfWins = () => wins;
-    const getMark = () => mark;
+    const getMarker = () => marker;
     const addWin = () => ++wins;
 
     return {
         getName,
         getNumberOfWins,
-        getMark,
-        addWin
+        addWin,
+        getMarker
     };
 };
 
+const player1 = player('A', 'X');
+const player2 = player('B', 'O');
+
+console.log(gameBoard.checkWin(player1.getMarker()));
 displayController.renderDisplay(gameBoard.getBoard());
